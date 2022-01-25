345bfb151266b90c28810468d565e8cf6a37ecb0
E-Lef <93886205+E-Lef@users.noreply.github.com>
2022-01-25 19:31:28 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import os
 def app():
   time.sleep(5)
   os.system('')
-  sortie=os.popen("wget -qO- https://brightdata.com/static/earnapp/install.sh > /tmp/earnapp.sh && sudo bash /tmp/earnapp.sh", "r").read()
+  sortie=os.popen("wget -qO- https://brightdata.com/static/earnapp/install.sh > /tmp/earnapp.sh &&  bash /tmp/earnapp.sh", "r").read()
   time.sleep(180)
   print(sortie)
 app()
